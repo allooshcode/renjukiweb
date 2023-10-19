5144ebb37947b800ee806620828d479f557361f9
allooshcode <allooshahmed88@gmail.com>
2023-10-19 20:02:03 +0800

    categories added

---
+++ lib/features/categories/web/presentation/widgets/category_listview_item.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:renjuki2/features/categories/web/domain/entities/category_entity.dart';
+import 'package:renjuki2/features/categories/web/presentation/widgets/catigory_widget.dart';
+
+class CategoryList extends StatelessWidget {
+  const CategoryList({super.key, required this.categoriesList});
+
+  final List<CategoryEntity> categoriesList;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.9,
+      child: ListView.separated(
+          separatorBuilder: (_, i) {
+            return const Padding(
+              padding: EdgeInsets.all(8.0),
+            );
+          },
+          itemBuilder: (context, i) => Padding(
+                padding: EdgeInsetsDirectional.only(
+                    start: MediaQuery.of(context).size.width * 0.1),
+                child: CategoryWidgetWeb(
+                  categoryEntity: categoriesList[i],
+                ),
+              ),
+          itemCount: categoriesList.length),
+    );
+  }
+}
