6590a915874129a3a010d679e48e151b9ada52bd
allooshcode <allooshahmed88@gmail.com>
2023-11-02 20:09:33 +0800

    new product

--- lib/features/productspage/data/data_sources/remote_data/product_data_remote_source.dart
+++ lib/features/productspage/data/data_sources/remote_data/product_data_remote_source.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 import 'package:dartz/dartz_unsafe.dart';
 import 'package:equatable/equatable.dart';
@@ -27,6 +29,78 @@ class ProductDataRemoteSource extends Equatable {
       return left(ServerFuilure(err.toString()));
     }
   }
+
+  Future<Either<Failure, bool>> createProduct(ProductModel product, File? imageFile, bool imageChanged,
+      bool itemUpdated, bool isBrowser) async{
+    try {
+      if (imageFile != null) {
+        if (imageChanged) {
+          if (!isBrowser) {
+            final ref = firebase_storage.FirebaseStorage.instance
+                .ref()
+                .child('productImages')
+                .child('${product.productName}.jpg');
+
+            await ref.putFile(imageFile);
+            imageUrl = await ref.getDownloadURL();
+          }
+        }
+      }
+
+      product.imageUrl = imageUrl;
+      if (itemUpdated) {
+        await fireStore.collection('Products').doc(product.productID).update(
+          {
+            Product.ID: product.productID,
+            Product.NAME: product.productName,
+            Product.DESCRIPTION: product.description,
+            Product.PRICE: product.price,
+            Product.IMAGEURL: imageUrl,
+            Product.CATEGORY: product.category,
+            Product.AVAILABLECOUNT: product.availableCount,
+            Product.SALE: product.sale,
+            Product.FEATURED: product.featured,
+            Product.FAVORITE: product.favorite,
+            Product.Flavour: product.flavours,
+            Product.SIZES: product.sizes,
+            Product.SOLD: product.sold,
+            Product.OFFER: product.offer,
+            Product.WEIGHT: product.weight.toString()
+          },
+        ).then((value) => imageUrl = null);
+      } else {
+        final id = uuid.v1();
+        await fireStore.collection('Products').doc(id).set({
+          Product.ID: id,
+          Product.NAME: product.productName,
+          Product.DESCRIPTION: product.description,
+          Product.PRICE: product.price,
+          Product.IMAGEURL: imageUrl,
+          Product.CATEGORY: product.category,
+          Product.AVAILABLECOUNT: product.availableCount,
+          Product.SALE: product.sale,
+          Product.OFFER: product.offer,
+          Product.FEATURED: product.featured,
+          Product.FAVORITE: product.favorite,
+          Product.Flavour: product.flavours,
+          Product.SIZES: product.sizes,
+          Product.SOLD: product.sold,
+          Product.WEIGHT: product.weight.toString()
+        }).then((value) => imageUrl = null);
+      }
+      notifyListeners();
+      //final url
+    } on FirebaseException catch (e) {
+      throw e.code;
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+  }
+
+
+
+
 
   @override
   // TODO: implement props
